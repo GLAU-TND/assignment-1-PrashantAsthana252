cfd808ff75baccb595dcc890aed4f8ad79e026f3
Prashant Asthana <prashant.asthana_cs18@gla.ac.in>
2020-02-24 22:14:59 +0530

    loop created

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,24 +8,6 @@ public class Main {
         Menu menu = new Menu();
         while(flag){
             int n = menu.showChoice();
-            switch(n){
-                case 1:
-                    menu.addChoice();
-                    break;
-                case 2:
-                    menu.viewChoice();
-                    break;
-                case 3:
-                    menu.searchChoice();
-                    break;
-                case 4:
-                    menu.deleteChoice();
-                    break;
-                case 5:
-                    menu.exitChoice();
-                    flag=false;
-                    break;
-            }
 
         }
 
